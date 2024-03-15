95b5eca310bce7461119c6d582b5f26364a457c2
1longg <lbnlong109@gmail.com>
2024-03-15 19:50:36 +0700

    21522300-Le Ba Nhat Long

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace TeamWork1_Group7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtOutput = new TextBox();
+            so7 = new Button();
+            so8 = new Button();
+            so9 = new Button();
+            so4 = new Button();
+            so5 = new Button();
+            so6 = new Button();
+            so1 = new Button();
+            so2 = new Button();
+            so3 = new Button();
+            dau_cham = new Button();
+            cong = new Button();
+            tru = new Button();
+            nhan = new Button();
+            chia_nguyen = new Button();
+            xoa_1 = new Button();
+            xoa_all = new Button();
+            so0 = new Button();
+            chia_du = new Button();
+            bang = new Button();
+            SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            txtOutput.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtOutput.Location = new Point(12, 41);
+            txtOutput.Name = "txtOutput";
+            txtOutput.ReadOnly = true;
+            txtOutput.Size = new Size(403, 33);
+            txtOutput.TabIndex = 0;
+            // 
+            // so7
+            // 
+            so7.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so7.Location = new Point(26, 100);
+            so7.Name = "so7";
+            so7.Size = new Size(53, 52);
+            so7.TabIndex = 1;
+            so7.Text = "7";
+            so7.UseVisualStyleBackColor = true;
+            so7.Click += so7_Click;
+            // 
+            // so8
+            // 
+            so8.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so8.Location = new Point(105, 100);
+            so8.Name = "so8";
+            so8.Size = new Size(53, 52);
+            so8.TabIndex = 2;
+            so8.Text = "8";
+            so8.UseVisualStyleBackColor = true;
+            so8.Click += so8_Click;
+            // 
+            // so9
+            // 
+            so9.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so9.Location = new Point(193, 100);
+            so9.Name = "so9";
+            so9.Size = new Size(53, 52);
+            so9.TabIndex = 3;
+            so9.Text = "9";
+            so9.UseVisualStyleBackColor = true;
+            so9.Click += so9_Click;
+            // 
+            // so4
+            // 
+            so4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so4.Location = new Point(26, 183);
+            so4.Name = "so4";
+            so4.Size = new Size(53, 52);
+            so4.TabIndex = 4;
+            so4.Text = "4";
+            so4.UseVisualStyleBackColor = true;
+            so4.Click += so4_Click;
+            // 
+            // so5
+            // 
+            so5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so5.Location = new Point(105, 183);
+            so5.Name = "so5";
+            so5.Size = new Size(53, 52);
+            so5.TabIndex = 5;
+            so5.Text = "5";
+            so5.UseVisualStyleBackColor = true;
+            so5.Click += so5_Click;
+            // 
+            // so6
+            // 
+            so6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so6.Location = new Point(193, 183);
+            so6.Name = "so6";
+            so6.Size = new Size(53, 52);
+            so6.TabIndex = 6;
+            so6.Text = "6";
+            so6.UseVisualStyleBackColor = true;
+            so6.Click += so6_Click;
+            // 
+            // so1
+            // 
+            so1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so1.Location = new Point(26, 263);
+            so1.Name = "so1";
+            so1.Size = new Size(53, 52);
+            so1.TabIndex = 7;
+            so1.Text = "1";
+            so1.UseVisualStyleBackColor = true;
+            so1.Click += so1_Click;
+            // 
+            // so2
+            // 
+            so2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so2.Location = new Point(105, 263);
+            so2.Name = "so2";
+            so2.Size = new Size(53, 52);
+            so2.TabIndex = 8;
+            so2.Text = "2";
+            so2.UseVisualStyleBackColor = true;
+            so2.Click += so2_Click;
+            // 
+            // so3
+            // 
+            so3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so3.Location = new Point(193, 263);
+            so3.Name = "so3";
+            so3.Size = new Size(53, 52);
+            so3.TabIndex = 9;
+            so3.Text = "3";
+            so3.UseVisualStyleBackColor = true;
+            so3.Click += so3_Click;
+            // 
+            // dau_cham
+            // 
+            dau_cham.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dau_cham.Location = new Point(193, 349);
+            dau_cham.Name = "dau_cham";
+            dau_cham.Size = new Size(53, 52);
+            dau_cham.TabIndex = 10;
+            dau_cham.Text = ".";
+            dau_cham.UseVisualStyleBackColor = true;
+            dau_cham.Click += dau_cham_Click;
+            // 
+            // cong
+            // 
+            cong.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cong.Location = new Point(275, 349);
+            cong.Name = "cong";
+            cong.Size = new Size(53, 52);
+            cong.TabIndex = 11;
+            cong.Text = "+";
+            cong.UseVisualStyleBackColor = true;
+            cong.Click += cong_Click;
+            // 
+            // tru
+            // 
+            tru.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tru.Location = new Point(275, 263);
+            tru.Name = "tru";
+            tru.Size = new Size(53, 52);
+            tru.TabIndex = 12;
+            tru.Text = "-";
+            tru.UseVisualStyleBackColor = true;
+            tru.Click += tru_Click;
+            // 
+            // nhan
+            // 
+            nhan.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            nhan.Location = new Point(275, 183);
+            nhan.Name = "nhan";
+            nhan.Size = new Size(53, 52);
+            nhan.TabIndex = 13;
+            nhan.Text = "*";
+            nhan.UseVisualStyleBackColor = true;
+            nhan.Click += nhan_Click;
+            // 
+            // chia_nguyen
+            // 
+            chia_nguyen.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chia_nguyen.Location = new Point(275, 100);
+            chia_nguyen.Name = "chia_nguyen";
+            chia_nguyen.Size = new Size(53, 52);
+            chia_nguyen.TabIndex = 14;
+            chia_nguyen.Text = "/";
+            chia_nguyen.UseVisualStyleBackColor = true;
+            chia_nguyen.Click += chia_nguyen_Click;
+            // 
+            // xoa_1
+            // 
+            xoa_1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            xoa_1.Location = new Point(352, 100);
+            xoa_1.Name = "xoa_1";
+            xoa_1.Size = new Size(53, 52);
+            xoa_1.TabIndex = 15;
+            xoa_1.Text = "CE";
+            xoa_1.UseVisualStyleBackColor = true;
+            xoa_1.Click += xoa_1_Click;
+            // 
+            // xoa_all
+            // 
+            xoa_all.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            xoa_all.Location = new Point(352, 183);
+            xoa_all.Name = "xoa_all";
+            xoa_all.Size = new Size(53, 52);
+            xoa_all.TabIndex = 16;
+            xoa_all.Text = "C";
+            xoa_all.UseVisualStyleBackColor = true;
+            xoa_all.Click += xoa_all_Click;
+            // 
+            // so0
+            // 
+            so0.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            so0.Location = new Point(26, 349);
+            so0.Name = "so0";
+            so0.Size = new Size(132, 52);
+            so0.TabIndex = 17;
+            so0.Text = "0";
+            so0.UseVisualStyleBackColor = true;
+            so0.Click += so0_Click;
+            // 
+            // chia_du
+            // 
+            chia_du.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chia_du.Location = new Point(352, 263);
+            chia_du.Name = "chia_du";
+            chia_du.Size = new Size(53, 52);
+            chia_du.TabIndex = 18;
+            chia_du.Text = "%";
+            chia_du.UseVisualStyleBackColor = true;
+            chia_du.Click += chia_du_Click;
+            // 
+            // bang
+            // 
+            bang.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bang.Location = new Point(352, 349);
+            bang.Name = "bang";
+            bang.Size = new Size(53, 52);
+            bang.TabIndex = 19;
+            bang.Text = "=";
+            bang.UseVisualStyleBackColor = true;
+            bang.Click += bang_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(427, 450);
+            Controls.Add(bang);
+            Controls.Add(chia_du);
+            Controls.Add(so0);
+            Controls.Add(xoa_all);
+            Controls.Add(xoa_1);
+            Controls.Add(chia_nguyen);
+            Controls.Add(nhan);
+            Controls.Add(tru);
+            Controls.Add(cong);
+            Controls.Add(dau_cham);
+            Controls.Add(so3);
+            Controls.Add(so2);
+            Controls.Add(so1);
+            Controls.Add(so6);
+            Controls.Add(so5);
+            Controls.Add(so4);
+            Controls.Add(so9);
+            Controls.Add(so8);
+            Controls.Add(so7);
+            Controls.Add(txtOutput);
+            Name = "Form1";
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtOutput;
+        private Button so7;
+        private Button so8;
+        private Button so9;
+        private Button so4;
+        private Button so5;
+        private Button so6;
+        private Button so1;
+        private Button so2;
+        private Button so3;
+        private Button dau_cham;
+        private Button cong;
+        private Button tru;
+        private Button nhan;
+        private Button chia_nguyen;
+        private Button xoa_1;
+        private Button xoa_all;
+        private Button so0;
+        private Button chia_du;
+        private Button bang;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,9 @@
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TeamWork1_Group7
+{
+
+}
+
